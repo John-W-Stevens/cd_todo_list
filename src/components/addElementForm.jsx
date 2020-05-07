f0f3b9c351cc521acdae1828508b56c1941f92cc
John Stevens <john.william.stevens1@gmail.com>
2020-05-07 13:40:04 -0700

    Added ref hook to AddElement component which: 1. clears the input field on form submission 2. refocuses the input field on form submission

--- src/components/addElementForm.jsx
+++ src/components/addElementForm.jsx
@@ -1,4 +1,11 @@
-import React from "react"
+import React, { useRef } from "react"
+
+// clear the input field for an uncontrolled (stateless element) using ref:
+// 1. import { useRef }
+// 2. const refContainer = useRef(initialState)
+// 3. add ref={refContainer} to the input field
+// 4. onSubmit/onClick -> refContainer.current.value = ""
+// 5. To focus back onto the input field -> refContainer.current.focus()
 
 
 const AddElementForm = props => {
@@ -9,14 +16,18 @@ const AddElementForm = props => {
         isComplete: false
     };
 
+    const refContainer = useRef("")
+
+
     const onChange = (e) => {
         element.name = e.target.value
     };
     const onSubmit = (e) => {
         e.preventDefault()
         setElements( [...elements, element] )
-        
-    }
+        refContainer.current.value = ""
+        refContainer.current.focus()
+        }
 
     return (
         <div className="row">
@@ -24,7 +35,7 @@ const AddElementForm = props => {
                 <form onSubmit = { onSubmit }>
                     <div className="form-group">
                         <label>Add element:</label>
-                        <input onChange = { onChange } type="text" name="input" className="form-control"></input>
+                        <input onChange = { onChange } ref={refContainer} id="elementInput" type="abc" name="input" className="form-control"></input>
                     </div>
                     <div className="form-group text-right">
                         <button className="btn btn-primary btn-sm">Add an element</button>
